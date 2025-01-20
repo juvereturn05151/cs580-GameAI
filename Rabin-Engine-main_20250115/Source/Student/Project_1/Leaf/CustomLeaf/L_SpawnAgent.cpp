d1c6146da7eba156349ad73cbeb11e85808880da
Ju-ve Chan <juve.chankasemporn@digipen.edu>
2025-01-20 05:32:10 -0800

    add goto target

--- Rabin-Engine-main_20250115/Source/Student/Project_1/Leaf/CustomLeaf/L_SpawnAgent.cpp
+++ Rabin-Engine-main_20250115/Source/Student/Project_1/Leaf/CustomLeaf/L_SpawnAgent.cpp
@@ -2,19 +2,20 @@
 #include "L_SpawnAgent.h"
 #include "Projects/ProjectOne.h"
 #include "CustomScript/GlobalBlackboard.h"
+#include "Agent/BehaviorAgent.h"
 L_SpawnAgent::L_SpawnAgent()
 {}
 
 void L_SpawnAgent::on_enter()
 {
     // 3. Create the agent, giving it the correct AgentModel type.
-    auto tree = agents->create_behavior_agent("ExampleAgent2", BehaviorTreeTypes::GoldbergBT, Agent::AgentModel::Man);
+    auto goldberg = agents->create_behavior_agent("ExampleAgent2", BehaviorTreeTypes::GoldbergBT, Agent::AgentModel::Man);
     
-    tree->set_position(Vec3(80,0, 20));
-    
+    goldberg->set_position(Vec3(80,0, 20));
+    goldberg->get_blackboard().set_value("target", Vec3(0, 0, 50));
     // 5. (optional) Set other aspects to make it start out correctly
-    tree->set_color(Vec3(1.0, 1.0, 0));   // Set the tree to green
-
+    goldberg->set_color(Vec3(1.0, 1.0, 0));   // Set the tree to green
+    GlobalBlackboard::get_instance().set_value("CanSpawnPyro", false);
     BehaviorNode::on_leaf_enter();
 
     on_success();
