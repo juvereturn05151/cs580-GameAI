ac22ea3dae9702abdc4e1fb9f8f7acce1ad2a037
Ju-ve Chan <juve.chankasemporn@digipen.edu>
2025-02-15 01:44:29 -0800

    Implement rubberbanding

--- Rabin-Engine-main_20250115/Source/Student/Project_2/P2_Pathfinding.h
+++ Rabin-Engine-main_20250115/Source/Student/Project_2/P2_Pathfinding.h
@@ -46,6 +46,8 @@ public:
     float heuristic(const GridPos& a, const GridPos& b, Heuristic heuristic);
     std::vector<GridPos> get_neighbors(const GridPos& pos);
     std::vector<Vec3> reconstruct_path(std::unordered_map<GridPos, GridPos, GridPosHash>& cameFrom, GridPos start, GridPos goal);
+    bool has_line_of_sight(const Vec3& start, const Vec3& end);
+    void apply_rubberbanding(std::vector<Vec3>& path);
     /* ************************************************** */
 
 private:
