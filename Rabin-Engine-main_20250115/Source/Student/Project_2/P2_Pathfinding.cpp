d861a231c85b7c48739e2cf20e60ab5735507f19
Ju-ve Chan <juve.chankasemporn@digipen.edu>
2025-02-20 02:22:21 -0800

    remove unused code

--- Rabin-Engine-main_20250115/Source/Student/Project_2/P2_Pathfinding.cpp
+++ Rabin-Engine-main_20250115/Source/Student/Project_2/P2_Pathfinding.cpp
@@ -56,35 +56,6 @@ Node* BucketPriorityQueue::Pop() {
     m_buckets[m_lowestNonEmptyBin].pop_back();
     m_numNodesTracked--;
     return node;
-
-
-
-    //Working Version
-    //if (Empty()) {
-    //    return nullptr;
-    //}
-    //// Make sure m_lowestNonEmptyBin points to a non-empty bucket.
-    //while (m_lowestNonEmptyBin < m_numBuckets && m_buckets[m_lowestNonEmptyBin].empty()) {
-    //    m_lowestNonEmptyBin++;
-    //}
-    //if (m_lowestNonEmptyBin >= m_numBuckets) {
-    //    return nullptr;
-    //}
-    //// Scan the bucket to find the node with the lowest finalCost.
-    //auto& bucket = m_buckets[m_lowestNonEmptyBin];
-    //int bestIndex = 0;
-    //float bestCost = bucket[0]->finalCost;
-    //for (size_t i = 1; i < bucket.size(); ++i) {
-    //    if (bucket[i]->finalCost < bestCost) {
-    //        bestCost = bucket[i]->finalCost;
-    //        bestIndex = i;
-    //    }
-    //}
-    //Node* bestNode = bucket[bestIndex];
-    //// Remove the best node from the bucket.
-    //bucket.erase(bucket.begin() + bestIndex);
-    //m_numNodesTracked--;
-    //return bestNode;
 }
 
 
